ea52b89ae6e8e46d7f3ee43e69316e0d6842aca9
sivakumarkalla16 <118175720+sivakumarkalla16@users.noreply.github.com>
2023-01-03 16:35:00 +0530

    commit3
    c

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 region = "ap-south-1"
 access_key = "AKIARDJD7CLXTQ3AMJON"
-secreet_key = "Fh7W37nx/eSS9+Vnw4DMGIZOdnsYVkd99nUs0Lwr"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
