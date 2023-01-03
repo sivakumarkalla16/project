b0253ed4e52dd4c9fa31b1b23ffbc6673676fd2d
sivakumarkalla16 <118175720+sivakumarkalla16@users.noreply.github.com>
2023-01-03 17:05:28 +0530

    commit4

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ access_key = "AKIARDJD7CLXTQ3AMJON"
 secret_key = "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"
 }
 
-resource "aws_instance" "one" {
+resource "aws_instance" "fuckk" {
 ami = "ami-0cca134ec43cf708f"
 instance_type = "t2.micro"
 tags = {
-Name = "terraform project"
+Name = "terratest"
 }
 } 
