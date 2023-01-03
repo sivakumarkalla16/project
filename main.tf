c3e398728bdfec48892d1c8799cefd552757bf0f
sivakumarkalla16 <118175720+sivakumarkalla16@users.noreply.github.com>
2023-01-03 20:24:25 +0530

    commit123

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "ami-0cca134ec43cf708f"
-  secret_key = "fVqflPYSFzdQcxm9ADob5/8jkfD01hF7y3QuyeFL"
+  access_key = "AKIARDJD7CLXRPSUQJEW"
+  secret_key = "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"
 }
 
-resource "aws_instance" "hello" {
+resource "aws_instance" "hellll" {
   ami           = "ami-0e6329e222e662a52"
   instance_type = "t2.micro"
   tags= {
-    Name = "lucky"
+    Name = "luckyy"
   }
 }
