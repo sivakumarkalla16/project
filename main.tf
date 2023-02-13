0da85439cc9769bdc3ea5201ea3afc656f3458b0
sivakumarkalla16 <118175720+sivakumarkalla16@users.noreply.github.com>
2023-02-13 22:52:10 +0530

    okkkkk

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIARDJD7CLXRPSUQJEW"
+  access_key = "AKIARDJD7CLXSMXPON7T"
   secret_key = "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"
 }
 
